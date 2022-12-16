1216cb8fee7cc7ba71e8108fd959df983af5952d
Oscar Chen <oscar.y.chen@gmail.com>
2022-12-15 21:53:19 -0700

    Set up demo Django models

---
+++ carmaker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarmakerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carmaker'
---
+++ carmaker/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Engine(models.Model):
+    name = models.CharField(max_length=128)
+    displacement = models.FloatField()
+
+
+class Project(models.Model):
+    code_name = models.CharField(max_length=128)
+
+
+class VehicleModel(models.Model):
+    model = models.CharField(max_length=256)
+    year = models.IntegerField()
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    maker = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    predecessor = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
+    engine_options = models.ManyToManyField(Engine)
+
+
+class Vehicle(models.Model):
+    VIN = models.CharField(max_length=18)
+    model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
+
+
+class Engineer(models.Model):
+    name = models.CharField(max_length=128)
+    works_on = models.ManyToManyField(VehicleModel)
